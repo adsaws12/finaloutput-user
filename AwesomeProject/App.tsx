98eb1e94715084e33ea8cc77a9eadb2ba372a505
Melvin <melvin@bbo.com.ph>
2019-12-03 18:28:49 +0800

    initial commit

---
+++ AwesomeProject/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import HomeScreen from './components/HomeScreen';
+
+export default function App() {
+  return (
+    <HomeScreen />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
